0d535648aabc125f6912babdaa4e643e88eeb711
timiajayi <timiajayi2001@gmail.com>
2024-12-05 14:18:59 +0100

    fix

--- config/ldap.php
+++ config/ldap.php
@@ -4,5 +4,5 @@ define('LDAP_PORT', 389);
 define('LDAP_DN', 'DC=sevenup,DC=org'); // Search base DN
 define('LDAP_USER_SEARCH_BASE', 'OU=Service Accounts,OU=SBC.Management,DC=sevenup,DC=org'); // Search path for users
 define('LDAP_BIND_DN', 'CN=SA-ferrum gate,OU=Service Accounts,OU=SBC.Management,DC=sevenup,DC=org'); // Admin bind DN
-define('LDAP_BIND_PASS', 'r9T6nL@xshL&TCy7nWL&ZAK8P'); // Admin bind password
+define('LDAP_BIND_PASS', ''); // Admin bind password
 ?>
