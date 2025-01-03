2374a11c403c48140e798b4857c0370480570584
timiajayi <timiajayi2001@gmail.com>
2025-01-03 15:06:19 +0100

    groups

--- v2/config/ldap.php
+++ v2/config/ldap.php
@@ -3,7 +3,7 @@
 return [
     'host' => env('LDAP_HOST', '10.10.15.50'),
     'username' => env('LDAP_USERNAME', "CN=SA-ferrum gate,OU=Service Accounts,OU=SBC.Management,DC=sevenup,DC=org"),
-    'password' => env('LDAP_PASSWORD', "k2X!t#WyF@c5"),
+    'password' => env('LDAP_PASSWORD', ""),
     'port' => env('LDAP_PORT', 389),
     'base_dn' => env('LDAP_BASE_DN', "DC=sevenup,DC=org"),
     'user_search_base' => env('LDAP_USER_SEARCH_BASE', "DC=sevenup,DC=org"),
